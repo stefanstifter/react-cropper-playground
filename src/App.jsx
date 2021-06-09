bd0ec268ec7e4d3f80ce3faaf037a19cb50f82a2
stefanstifter <stefan.stifter@saswebdev.com>
2021-06-09 09:47:18 +0200

    Utilize tailwindcss from cdn, remove styles

--- src/App.jsx
+++ src/App.jsx
@@ -1,42 +1,11 @@
-import React, { useState } from 'react'
-import logo from './logo.svg'
-import './App.css'
+import React from 'react'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Hello Vite + React!</p>
-        <p>
-          <button type="button" onClick={() => setCount((count) => count + 1)}>
-            count is: {count}
-          </button>
-        </p>
-        <p>
-          Edit <code>App.jsx</code> and save to test HMR updates.
-        </p>
-        <p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-          {' | '}
-          <a
-            className="App-link"
-            href="https://vitejs.dev/guide/features.html"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Vite Docs
-          </a>
-        </p>
+    <div className="max-w-7xl mx-auto sm:px-6 lg:px-8">
+      <header className="pt-6">
+          <h1 className="text-2xl font-bold">React Cropper Playground</h1>
+          <p>Test a cropper</p>
       </header>
     </div>
   )
